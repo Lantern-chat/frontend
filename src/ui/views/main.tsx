87eab3707325ff4c3f73cb6f884aa4d1a028ce74
novacrazy <novacrazy@gmail.com>
2020-12-27 11:54:37 -0600

    demo async code

--- src/ui/views/main.tsx
+++ src/ui/views/main.tsx
@@ -1,8 +1,9 @@
 import React, { useEffect } from "react";
 import { fetch } from "../../client/fetch";
-import { delay } from "../../client/util";
+import { delay, wrapPromise } from "../../client/util";
 import { i18n, Translation as T } from "../i18n";
 import { Timestamp } from "../components/common/timestamp";
+import { BuildConfig } from "src/client/interfaces";
 
 //function fetchBuild() {
 //    return fetch("/api/v1/build").then((xhr) => (
@@ -10,21 +11,36 @@ import { Timestamp } from "../components/common/timestamp";
 //    ));
 //}
 
+function displayBuild(build: BuildConfig | null) {
+    if(build) {
+        return <>{JSON.stringify(build)}</>;
+    } else {
+        return null;
+    }
+}
+
 export default function MainView() {
-    let [build, setBuild] = React.useState({});
+    // Initialize the state with a dummy read function, as it's called on the first render and must be valid below
+    // Setting it to return null will skip rendering any part of it
+    let [buildRead, setBuild] = React.useState({ read: () => null } as any);
     let [startTransition, isPending] = React.unstable_useTransition();
 
+    /* Calling read here will eith throw the promise, or return the resolved data from above */
+    let build = displayBuild(buildRead.read());
 
-    useEffect(() => {
-        delay(1000).then(() => fetch("/api/v1/build").then((xhr) => setBuild(xhr.response)));
-    }, []);
+    // Setup transition, which will track `setBuild`'s call
+    let onMouseUp = () => startTransition(() => {
+        // hypothetical loading that returns the data
+        let promise = delay(1000).then(() => fetch("/api/v1/build").then((xhr) => xhr.response));
+
+        // Ensure that the object given as the build state is a "wrapped" promise
+        setBuild(wrapPromise(promise))
+    });
 
     return (
         <div>
-            <Timestamp time={Date.now()} />
-            <br />
-            {isPending ? "Loading..." : null}
-            {JSON.stringify(build)}
+            <button disabled={isPending} onMouseUp={onMouseUp}>Load Content</button>
+            {isPending ? "Loading..." : build}
         </div>
     );
 };
