35fa8fa0efb1bdfa6182c29314562e3121584fb0
novacrazy <novacrazy@gmail.com>
2021-01-12 18:33:30 -0600

    Add firefly effect

--- src/ui/views/register.tsx
+++ src/ui/views/register.tsx
@@ -6,13 +6,42 @@ import "./register.scss";
 
 import lantern from "../assets/lantern_wax.svg";
 
+const Firefly = React.memo(() => {
+    let offset_x = Math.random() * 100;
+    let offset_y = Math.random() * 100;
+    let offset_a = Math.random() * 360;
+
+    let idx = Math.floor(Math.random() * 2);
+    let className = "ln-random-walk-" + idx;
+
+    let style = {
+        position: "absolute" as any, // TypeScript is bugged
+        left: offset_x + '%',
+        top: offset_y + '%',
+        animationDuration: (Math.random() + 1) + 's',
+        transform: `rotate(${offset_a}deg)`,
+        animationDelay: (Math.random() * 2) + 's',
+    };
+
+    return (<span className={"ln-firefly ln-blinking " + className} style={style} />)
+});
+
+
 // TODO: Abstract form groups and inputs into components
 export default class RegisterView extends React.Component {
     render() {
         return (
             <>
                 <div className="ln-center ln-register">
-                    <div className="ln-secondary-surface-background ln-register-container">
+                    {(() => {
+                        let children = [];
+                        for(let i = 0; i < 20; i++) {
+                            children.push(<Firefly key={i} />);
+                        }
+                        return children;
+                    })()}
+                    <div className="ln-secondary-surface-background ln-register-container" style={{ zIndex: 1 }}>
+
                         <form className="ln-primary-text-color">
                             <div className="ln-center">
                                 <h2>Register</h2>
@@ -49,12 +78,11 @@ export default class RegisterView extends React.Component {
                                 <Link to={"/login"}>Go to Login</Link>
                             </div>
                         </form>
-                        <div className="ln-logo">
+                        <div className="ln-logo" >
                             <img src={lantern}></img>
                         </div>
                     </div>
                 </div>
-                <div className="ln-fireflies-1"></div>
             </>
         );
     }
