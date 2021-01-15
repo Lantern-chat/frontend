73962997565e9b5db5b3414c5597004c01db89db
novacrazy <novacrazy@gmail.com>
2021-01-15 14:57:18 -0600

    Start on form validation

---
+++ src/ui/components/form/index.tsx
@@ -0,0 +1,61 @@
+import React, { useMemo, useRef, useState } from "react";
+
+import "./form.scss";
+
+interface IFormGroupProps {
+    children: React.ReactNode
+}
+
+interface IFormLabelProps {
+    children: React.ReactNode,
+    htmlFor: string,
+}
+
+interface IFormInputProps {
+    type: string,
+    name: string,
+    placeholder?: string,
+    required?: boolean,
+    validator?: (value: string) => boolean,
+}
+
+export function FormGroup(props: IFormGroupProps) {
+    return <div className="ln-form-group" {...props} />
+};
+
+export function FormLabel(props: IFormLabelProps) {
+    return <label className="ln-form-label" {...props} />;
+};
+
+export function FormInput(props: IFormInputProps) {
+    let input: React.RefObject<HTMLInputElement> = useRef(null);
+    let [value, setValue] = useState<string | null>(null);
+
+    let classNames = "ln-form-control-wrapper";
+
+    let isValid = null;
+
+    if(value != null) {
+        if(props.validator) isValid = props.validator(value);
+    }
+
+    if(isValid != null) {
+        if(isValid) {
+            classNames += ' ln-success';
+        } else {
+            classNames += ' ln-error';
+        }
+    }
+
+    return (
+        <span className={classNames}>
+            <input className="ln-form-control ln-tertiary-surface-background ln-secondary-surface-border ln-primary-text-color"
+                ref={input}
+                type={props.type}
+                name={props.name}
+                placeholder={props.placeholder}
+                required={props.required}
+                onChange={(e) => setValue(e.target.value)} />
+        </span>
+    );
+};
--- src/ui/views/register.tsx
+++ src/ui/views/register.tsx
@@ -3,10 +3,19 @@ import React from "react";
 import { Link } from "react-router-dom";
 
 import { Fireflies } from "../components/login/fireflies";
+import { FormGroup, FormLabel, FormInput } from "../components/form";
 
 import "./register.scss";
 
 import lantern from "../assets/lantern.svg";
+
+function hasLength(length: number): (value: string) => boolean {
+    return (value: string) => value.length > length;
+}
+
+function validateEmail(value: string): boolean {
+    return /^[^@\s]+@[^@\s]+\.[^@\s]+$/.test(value);
+}
 
 // TODO: Abstract form groups and inputs into components
 export const RegisterView = () => {
@@ -20,43 +29,33 @@ export const RegisterView = () => {
                             <div id="title">
                                 <h2>Register</h2>
                             </div>
-                            <div className="ln-form-group">
-                                <label className="ln-form-label" htmlFor="email">Email Address</label>
-                                <span className="ln-form-control-wrapper">
-                                    <input className="ln-form-control ln-tertiary-surface-background ln-secondary-surface-border ln-primary-text-color"
-                                        type="email" name="email" placeholder="example@example.com" required />
-                                </span>
-                            </div>
-                            <div className="ln-form-group">
-                                <label className="ln-form-label" htmlFor="email">Username</label>
-                                <span className="ln-form-control-wrapper">
-                                    <input className="ln-form-control ln-tertiary-surface-background ln-secondary-surface-border ln-primary-text-color"
-                                        type="text" name="username" placeholder="username" required />
-                                </span>
-                            </div>
-                            <div className="ln-form-group">
-                                <label className="ln-form-label" htmlFor="email">Nickname</label>
-                                <span className="ln-form-control-wrapper">
-                                    <input className="ln-form-control ln-tertiary-surface-background ln-secondary-surface-border ln-primary-text-color"
-                                        type="text" name="nickname" placeholder="nickname" />
-                                </span>
-                            </div>
-                            <div className="ln-form-group">
-                                <label className="ln-form-label" htmlFor="email">Password</label>
-                                <span className="ln-form-control-wrapper">
-                                    <input className="ln-form-control ln-tertiary-surface-background ln-secondary-surface-border ln-primary-text-color"
-                                        type="password" name="password" placeholder="password" required />
-                                </span>
-                            </div>
-                            <div className="ln-form-group" style={{ display: 'flex', padding: '0 1em' }}>
-                                <button className="ln-btn ln-primary-text-color" style={{ marginRight: 'auto' }}>Submit</button>
-                                <Link to={"/login"} className="ln-btn ln-primary-text-color" >Go to Login</Link>
-                            </div>
-                            <div className="ln-form-group">
+                            <FormGroup>
+                                <FormLabel htmlFor="email">Email Address</FormLabel>
+                                <FormInput type="email" name="email" placeholder="example@example.com" required validator={validateEmail} />
+                            </FormGroup>
+                            <FormGroup>
+                                <FormLabel htmlFor="email">Username</FormLabel>
+                                <FormInput type="text" name="username" placeholder="username" required validator={hasLength(6)} />
+                            </FormGroup>
+                            <FormGroup>
+                                <FormLabel htmlFor="email">Nickname</FormLabel>
+                                <FormInput type="text" name="nickname" placeholder="nickname" />
+                            </FormGroup>
+                            <FormGroup>
+                                <FormLabel htmlFor="email">Password</FormLabel>
+                                <FormInput type="password" name="password" placeholder="password" required validator={hasLength(8)} />
+                            </FormGroup>
+                            <FormGroup>
+                                <div style={{ display: 'flex', padding: '0 1em' }}>
+                                    <button className="ln-btn ln-primary-text-color" style={{ marginRight: 'auto' }}>Submit</button>
+                                    <Link to={"/login"} className="ln-btn ln-primary-text-color" >Go to Login</Link>
+                                </div>
+                            </FormGroup>
+                            <FormGroup>
                                 <p className="ln-form-text">
                                     By registering, you agree to our
                                 </p>
-                            </div>
+                            </FormGroup>
                         </div>
                         <div className="ln-logo" >
                             <img src={lantern}></img>
