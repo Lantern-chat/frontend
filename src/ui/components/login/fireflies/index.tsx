733fe9b42512171d13dd6e21b1568f9128616375
novacrazy <novacrazy@gmail.com>
2021-01-15 09:37:48 -0600

    More firefly tweaks

--- src/ui/components/login/fireflies/index.tsx
+++ src/ui/components/login/fireflies/index.tsx
@@ -10,6 +10,9 @@ interface IFireflyProps {
     count: number,
 }
 
+const FIREFLY_RADIUS: number = 16;
+const FIREFLY_WIDTH: number = FIREFLY_RADIUS * 2;
+
 interface IFirefly {
     offset: number,
     size: number,
@@ -17,8 +20,10 @@ interface IFirefly {
     a: number,
     // Target angle
     ta: number,
-    pos: [number, number],
-    vel: [number, number],
+    px: number,
+    py: number,
+    vx: number,
+    vy: number,
 }
 
 interface IFireflyState {
@@ -29,6 +34,8 @@ interface IFireflyState {
     time?: number,
     // Array of fireflies
     ff: IFirefly[],
+    // mouse position
+    m: [number, number, boolean],
 }
 
 function init_fireflies(props: IFireflyProps): IFireflyState {
@@ -40,12 +47,13 @@ function init_fireflies(props: IFireflyProps): IFireflyState {
             size: random() * 0.7 + 0.3,
             a,
             ta: a,
-            pos: [random() * window.innerWidth, random() * window.innerHeight],
-            vel: [0, 0],
+            px: random() * window.innerWidth,
+            py: random() * window.innerHeight,
+            vx: 0, vy: 0,
         });
     }
 
-    return { ff, paused: false };
+    return { ff, paused: false, m: [1e9, 1e9, false] };
 }
 
 const MAX_SPEED = 100;
@@ -63,7 +71,7 @@ function smoothstep(x: number): number {
 }
 
 function squine3(x0: number): number {
-    let quad = (x: number) => (1 + cbrt(-1 + x * x * x));
+    let quad = (x: number) => (1 + cbrt(x * x * x - 1));
     let semi = (x: number) => (x <= 1 ? quad(x) : (2 - quad(-x + 2)));
     let full = (x: number) => (x <= 2 ? semi(x) : semi(-x + 4));
 
@@ -74,6 +82,32 @@ function broad_sine5(x: number): number {
     let s = sin((x - 1) * PI * 0.5);
     return 1 + cbrt(abs(s)) * sign(s);
 }
+
+function repulse(x0: number, y0: number, x1: number, y1: number, w: number): [number, number, number] {
+    let dx = x0 - x1;
+    let dy = y0 - y1;
+    let d = sqrt(dx * dx + dy * dy);
+    return [smoothstep((w - d) / w), dx, dy];
+}
+
+function gaussian2(x: number, c: number): number {
+    return Math.pow(2, (x * x) / (-2 * c * c));
+}
+
+interface GradientStop {
+    x: number,
+    v: string,
+}
+
+// The radial gradient uses a Gaussian function for smoothness
+const GRADIENT: GradientStop[] = [];
+const NUM_STOPS: number = 8;
+for(let i = 0; i < NUM_STOPS; i++) { // Note this is not inclusive
+    let x = i / NUM_STOPS;
+    GRADIENT.push({ x, v: `rgba(255, 255, 0, ${gaussian2(x, 0.25)})` });
+}
+GRADIENT.push({ x: 1, v: 'rgba(0, 0, 0, 0)' }); // ensure it ends in black/transparent
+
 
 function render_fireflies(state: IFireflyState, canvas_ref: React.MutableRefObject<HTMLCanvasElement | null>, time: number) {
     if(!canvas_ref.current) { return; }
@@ -103,60 +137,73 @@ function render_fireflies(state: IFireflyState, canvas_ref: React.MutableRefObje
 
             // adjust velocity
             let v = dt * MAX_SPEED;
-            firefly.vel[0] += sin(firefly.a) * v;
-            firefly.vel[1] += cos(firefly.a) * v;
+            firefly.vx += sin(firefly.a) * v;
+            firefly.vy += cos(firefly.a) * v;
 
             // for every 0.4% of particles, apply some repulsion effects
             if(random() < 0.004) {
                 for(let other of state.ff) {
-                    let dx = firefly.pos[0] - other.pos[0];
-                    let dy = firefly.pos[1] - other.pos[1];
-                    let d = sqrt(dx * dx + dy * dy);
-
-                    // Smoothstep falloff
-                    let repulse = smoothstep((40 - d) / 40);
+                    let [r, dx, dy] = repulse(firefly.px, firefly.py, other.px, other.py, 40);
 
                     // computes a tiny parabola where the repulsive effect is propertional
                     // to difference in sizes, with max repulsion at equal sizes
                     let ds = 2 * abs(firefly.size - other.size);
-                    repulse *= repulse * max(0, 1 - ds * ds) * dt * 666 * random();
+                    r *= r * max(0, 1 - ds * ds) * dt * 666 * random();
 
                     // 20% of the time, attract them instead for a bit of unpredictability
                     if(random() < 0.2) {
-                        repulse *= -1;
+                        r *= -1;
                     }
 
-                    firefly.vel[0] += dx * repulse;
-                    firefly.vel[1] += dy * repulse;
+                    firefly.vx += dx * r;
+                    firefly.vy += dy * r;
                 }
             }
 
+            // TODO: Determine if click-attract is appealing
+            state.m[2] = false;
+
+            // Repel from mouse cursor
+            let [r, dx, dy] = repulse(firefly.px, firefly.py, state.m[0], state.m[1], 30 * firefly.size * (state.m[2] ? 10 : 1));
+            r *= r * dt * 1000 * (state.m[2] ? -1 : 1); // ^2 falloff + time domain
+            // accelerate away in the current direction a tiny bit
+            firefly.vx *= r * 0.1 + 1;
+            firefly.vy *= r * 0.1 + 1;
+            // repel away from the mouse
+            firefly.vx += dx * r * 0.9;
+            firefly.vy += dy * r * 0.9;
+
+
             // dampening
-            firefly.vel[0] = min(EJECT, max(-EJECT, firefly.vel[0] * 0.99));
-            firefly.vel[1] = min(EJECT, max(-EJECT, firefly.vel[1] * 0.99));
+            firefly.vx = min(EJECT, max(-EJECT, firefly.vx * 0.99));
+            firefly.vy = min(EJECT, max(-EJECT, firefly.vy * 0.99));
 
             // apply velocity to position
-            firefly.pos[0] += dt * firefly.vel[0];
-            firefly.pos[1] += dt * firefly.vel[1];
+            firefly.px += dt * firefly.vx;
+            firefly.py += dt * firefly.vy;
 
             // clamp position
-            firefly.pos[0] = min(max(0, firefly.pos[0]), canvas.width);
-            firefly.pos[1] = min(max(0, firefly.pos[1]), canvas.height);
+            firefly.px = min(max(0, firefly.px), canvas.width);
+            firefly.py = min(max(0, firefly.py), canvas.height);
 
             // if at border, apply repulsive force
-            if(firefly.pos[0] === 0) { firefly.vel[0] += EJECT; }
-            else if(firefly.pos[0] === canvas.width) { firefly.vel[0] -= EJECT; }
-            if(firefly.pos[1] === 0) { firefly.vel[1] += EJECT; }
-            else if(firefly.pos[1] === canvas.height) { firefly.vel[1] -= EJECT; }
+            if(firefly.px === 0) { firefly.vx += EJECT; }
+            else if(firefly.px === canvas.width) { firefly.vx -= EJECT; }
+            if(firefly.py === 0) { firefly.vy += EJECT; }
+            else if(firefly.py === canvas.height) { firefly.vy -= EJECT; }
         }
         ctx.resetTransform();
         ctx.clearRect(0, 0, canvas.width, canvas.height);
 
-        let gradient = ctx.createRadialGradient(0, 0, 0, 0, 0, 12);
+        let gradient = ctx.createRadialGradient(0, 0, 0, 0, 0, FIREFLY_RADIUS);
 
-        gradient.addColorStop(0, 'rgba(255, 255, 0, 0.9)');
-        gradient.addColorStop(0.2, 'rgba(255, 255, 0, 0.5)');
-        gradient.addColorStop(1, 'rgba(0, 0, 0, 0)');
+        for(let g of GRADIENT) {
+            gradient.addColorStop(g.x, g.v);
+        }
+
+        //gradient.addColorStop(0, 'rgba(255, 255, 0, 0.9)');
+        //gradient.addColorStop(0.2, 'rgba(255, 255, 0, 0.5)');
+        //gradient.addColorStop(1, 'rgba(0, 0, 0, 0)');
 
         ctx.fillStyle = gradient;
 
@@ -165,8 +212,8 @@ function render_fireflies(state: IFireflyState, canvas_ref: React.MutableRefObje
             // to avoid overlapping blinks with others
             ctx.globalAlpha = min(1, 0.1 + 0.45 * squine3((firefly.offset * 15.61803398 + time) * 0.5));
 
-            ctx.setTransform(firefly.size, 0, 0, firefly.size, firefly.pos[0], firefly.pos[1]);
-            ctx.fillRect(-12, -12, 24, 24);
+            ctx.setTransform(firefly.size, 0, 0, firefly.size, firefly.px, firefly.py);
+            ctx.fillRect(-FIREFLY_RADIUS, -FIREFLY_RADIUS, FIREFLY_WIDTH, FIREFLY_WIDTH);
         }
     }
 
@@ -175,6 +222,8 @@ function render_fireflies(state: IFireflyState, canvas_ref: React.MutableRefObje
 
     state.frame = requestAnimationFrame((new_time: number) => render_fireflies(state, canvas_ref, new_time));
 }
+
+const MOUSE_EVENTS = ['mousemove', 'movedown', 'mouseup'];
 
 // TODO: Check for reduce-motion
 export const Fireflies = React.memo((props: IFireflyProps) => {
@@ -188,13 +237,21 @@ export const Fireflies = React.memo((props: IFireflyProps) => {
                 firefly.ta += (random() * 2.0 - 1.0) * PI * 0.1;
             }
         }, 1 / ANGLE_INTERVAL);
-        let listener = () => { state.paused = isPageHidden(); state.just_unpaused = !state.paused; };
-        if(visibilityChange) { document.addEventListener(visibilityChange, listener); }
+        let mouse_listener = (e: MouseEvent) => { state.m = [e.x, e.y, e.buttons == 1]; }
+        for(let e of MOUSE_EVENTS) {
+            window.addEventListener(e, mouse_listener);
+        }
+        let hidden_listener = () => { state.paused = isPageHidden(); state.just_unpaused = !state.paused; };
+        if(visibilityChange) { document.addEventListener(visibilityChange, hidden_listener); }
         state.frame = requestAnimationFrame((time: number) => render_fireflies(state, canvas_ref, time));
         return () => {
-            clearInterval(interval);
+            // cancel animation first
             if(state.frame) cancelAnimationFrame(state.frame);
-            if(visibilityChange) document.removeEventListener(visibilityChange, listener);
+            clearInterval(interval);
+            if(visibilityChange) document.removeEventListener(visibilityChange, hidden_listener);
+            for(let e of MOUSE_EVENTS) {
+                window.removeEventListener(e, mouse_listener);
+            }
         }
     }, [props.count])
 
