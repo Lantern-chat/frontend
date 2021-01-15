2986eae55141fe640556ebcaa535e2c4402a4aa7
novacrazy <novacrazy@gmail.com>
2021-01-15 09:38:02 -0600

    Add some utils for checking visibility state

--- src/ui/utils.ts
+++ src/ui/utils.ts
@@ -29,3 +29,33 @@ export function wrapPromise<T>(promise: Promise<T>) {
         }
     };
 }
+
+export function isDescendant(parent: HTMLElement, child: HTMLElement): boolean {
+    var node = child.parentNode;
+    while(node != null) {
+        if(node == parent) {
+            return true;
+        }
+        node = node.parentNode;
+    }
+    return false;
+}
+
+export var visibilityChange: string | null;
+var hidden: string | null, d = document as any;
+if(typeof d.hidden !== "undefined") { // Opera 12.10 and Firefox 18 and later support
+    hidden = "hidden";
+    visibilityChange = "visibilitychange";
+} else if(typeof d.msHidden !== "undefined") {
+    hidden = "msHidden";
+    visibilityChange = "msvisibilitychange";
+} else if(typeof d.webkitHidden !== "undefined") {
+    hidden = "webkitHidden";
+    visibilityChange = "webkitvisibilitychange";
+} else {
+    console.log("Page visibility API is not available!");
+}
+
+export function isPageHidden(): boolean {
+    return hidden != null && d[hidden];
+}
