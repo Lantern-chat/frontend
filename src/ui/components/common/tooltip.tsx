31defdf147befcdc14e17b65e04b0addd3ba8425
novacrazy <novacrazy@gmail.com>
2021-02-04 03:38:41 -0600

    Force layout refresh for tooltips upon mount

--- src/ui/components/common/tooltip.tsx
+++ src/ui/components/common/tooltip.tsx
@@ -1,4 +1,4 @@
-import React, { useRef, useMemo, useState, useEffect } from "react";
+import React, { useRef, useMemo, useState, useEffect, useLayoutEffect } from "react";
 
 export interface ITooltipProps {
     children: React.ReactNode;
@@ -7,8 +7,11 @@ export interface ITooltipProps {
 }
 
 import "./tooltip.scss";
-export const Tooltip = (props: ITooltipProps) => {
+export const Tooltip = React.memo((props: ITooltipProps) => {
     let body = useRef<HTMLDivElement | null>(null);
+    let [, forceRender] = useState(false);
+
+    useLayoutEffect(() => forceRender(true), []);
 
     let { children, x = 0, y = 0 } = props;
 
@@ -40,57 +43,6 @@ export const Tooltip = (props: ITooltipProps) => {
         return {};
     }, [rect, props.x, props.y]);
 
-
-
-    /*
-    let [render, forceRender] = useState(0);
-
-    useEffect(() => {
-        let listener = () => forceRender(render + 1);
-        window.addEventListener('resize', listener);
-        return () => window.removeEventListener('resize', listener);
-    }, []);
-
-    let h = window.innerWidth;
-    let w = window.innerHeight;
-
-    let style = useMemo(() => {
-        if(rect != null) {
-            let { x, y, width, height } = rect;
-
-            console.log(rect, h, w);
-
-            let top_margin = h / 3;
-            let bottom_margin = h - top_margin;
-
-            let left_margin = w / 3;
-            let right_margin = w - left_margin;
-
-            let on_top = y < top_margin;
-            let on_bottom = y > bottom_margin;
-
-            let on_left = x < left_margin;
-            let on_right = x > right_margin;
-
-            let top = undefined;
-            if(on_top) {
-                top = 0;
-            } else if(on_bottom) {
-                top = 1;
-            } else {
-                top = '-50%';
-            }
-
-            return {
-                top,
-                left: '50%',
-            };
-        }
-
-        return {};
-    }, [rect, h, w]);
-    */
-
     return (
         <div className="ln-tooltip-anchor">
             <div ref={body} className="ln-tooltip-body" style={style}>
@@ -98,4 +50,4 @@ export const Tooltip = (props: ITooltipProps) => {
             </div>
         </div>
     )
-};
+});
