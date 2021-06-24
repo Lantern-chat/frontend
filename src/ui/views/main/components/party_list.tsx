74d9d06dd9f9a32a0c3c5ec39d8e8e4ee361b921
novacrazy <novacrazy@gmail.com>
2021-06-24 02:49:03 -0500

    Correct sorting of parties in party list

--- src/ui/views/main/components/party_list.tsx
+++ src/ui/views/main/components/party_list.tsx
@@ -1,6 +1,6 @@
 import React, { useContext } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { createStructuredSelector } from "reselect";
+import { createSelector, createStructuredSelector } from "reselect";
 
 import { fetch, XHRMethod } from "lib/fetch";
 import { fnv1a } from "lib/fnv";
@@ -14,51 +14,66 @@ import { setSession } from "state/action_creators/session";
 
 import LogoutIcon from "icons/glyphicons-pro/glyphicons-basic-2-4/svg/individual-svg/glyphicons-basic-432-log-out.svg";
 
+let sorted_party_selector = createSelector((state: RootState) => state.party.parties, parties => {
+    // this really just copies references into an array, so it should be fast
+    let party_array = Array.from(parties.values());
+    party_array.sort((a, b) => a.sort_order - b.sort_order);
+    return party_array;
+});
 
 let party_list_selector = createStructuredSelector({
-    parties: (state: RootState) => state.party.parties,
+    parties: sorted_party_selector,
     last_channel: (state: RootState) => state.party.last_channel,
     create_party_open: (state: RootState) => state.modals.create_party_open,
     auth: (state: RootState) => state.user.session?.auth,
+    active_party: (state: RootState) => state.history.parts[1] // /channels/:party_id/:channel_id
 });
+
+const Logout = () => {
+    let auth = useSelector((state: RootState) => state.user.session!.auth);
+
+    let dispatch = useDispatch();
+    let logout = () => dispatch(
+        fetch({
+            url: "/api/v1/user/@me",
+            method: XHRMethod.DELETE,
+            headers: { 'Authorization': 'Bearer ' + auth }
+        }).then(() => setSession(null))
+    );
+
+    return (
+        <div id="logout" onClick={logout} title="Logout">
+            <Glyphicon src={LogoutIcon} />
+        </div>
+    );
+};
 
 import "./party_list.scss";
 export const PartyList = React.memo(() => {
-    let active_party = ""; //useRouteMatch<{ party: string }>("/channels/:party")?.params.party;
-
+    let { create_party_open, parties, last_channel, active_party } = useSelector(party_list_selector);
     let dispatch = useDispatch();
-    let { create_party_open, parties, last_channel, auth } = useSelector(party_list_selector);
-
-    let logout = () => {
-        dispatch(
-            fetch({
-                url: "/api/v1/user/@me",
-                method: XHRMethod.DELETE,
-                headers: { 'Authorization': 'Bearer ' + auth }
-            }).then(() => setSession(null))
-        );
-    };
 
     let colors = ['goldenrod', 'royalblue', 'darkgreen', 'crimson'];
 
     return (
         <div className="ln-party-list__wrapper">
             <ol className="ln-party-list ln-scroll-y ln-scroll-y--invisible ln-scroll-fixed">
-                <li value={0} id="user-home" className={'@me' == active_party ? 'selected' : ''}>
+                <li id="user-home" className={'@me' == active_party ? 'selected' : ''}>
                     <Link href="/channels/@me">
                         <Avatar rounded text="@" username="Home" title="Home" />
                     </Link>
                 </li>
 
-                {Array.from(parties.values(), (party, i) => {
-                    let last = last_channel.get(party.id);
+                {parties.map((party, i) => {
+                    let last = last_channel.get(party.id),
+                        url = party.icon_id && `/avatars/${party.id}/${party.icon_id}`;
+
                     last = last ? '/' + last : '';
+
                     return (
-                        <li value={1 + party.sort_order} key={party.id}
-                            className={party.id == active_party ? 'selected' : undefined}>
+                        <li key={party.id} className={party.id == active_party ? 'selected' : undefined}>
                             <Link href={`/channels/${party.id}${last}`}>
-                                <Avatar rounded url={true ? undefined : `https://placekitten.com/${(i % 25) + 50}/${(i % 25) + 50}`}
-                                    text={party.name.charAt(0)}
+                                <Avatar rounded url={url} text={party.name.charAt(0)}
                                     username={party.name} title={party.name} backgroundColor={colors[fnv1a(party.id) % colors.length]} />
                             </Link>
                         </li>
@@ -71,12 +86,11 @@ export const PartyList = React.memo(() => {
                 </li>
             </ol>
 
-            <div id="logout" onClick={logout} title="Logout">
-                <Glyphicon src={LogoutIcon} />
-            </div>
+            <Logout />
         </div>
     );
 });
 if(__DEV__) {
     PartyList.displayName = "PartyList";
+    Logout.displayName = "Logout";
 }
