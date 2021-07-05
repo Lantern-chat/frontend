12d92170b9e594e64eb79345c5334dd4fe27a69d
novacrazy <novacrazy@gmail.com>
2021-07-05 09:32:34 -0500

    WIP user list

--- src/ui/views/main/components/party/member_list.tsx
+++ src/ui/views/main/components/party/member_list.tsx
@@ -1,7 +1,10 @@
+import { pickColorFromHash } from "lib/palette";
 import React from "react";
 import { useSelector } from "react-redux";
 import { createSelector, createStructuredSelector } from "reselect";
 import { RootState } from "state/root";
+
+import { Avatar } from "ui/components/common/avatar";
 
 let user_list_selector = createSelector(
     (state: RootState) => state.history.parts[1], // party_id
@@ -16,23 +19,34 @@ let user_list_selector = createSelector(
     }
 )
 
+let other_selector = createStructuredSelector({
+    is_light_theme: (state: RootState) => state.theme.is_light,
+});
+
 import "./member_list.scss";
 export const MemberList = React.memo(() => {
     let { members } = useSelector(user_list_selector);
+    let { is_light_theme } = useSelector(other_selector);
 
     let list = !members ? null : Array.from(members.values(), (member) => {
         let user = member.user!;
         let nick = member.nick || user.username;
 
         return (
-            <div key={user.id}>
-                {nick}
-            </div>
+            <li key={user.id} className="ln-member-list__item">
+                <div className="ln-member__avatar">
+                    <Avatar username={nick} text={nick.charAt(0)} backgroundColor={pickColorFromHash(user.id, is_light_theme)} />
+                    <div className="ln-member__status"><span /></div>
+                </div>
+                <div className="ln-member__name">
+                    <span>{nick}</span>
+                </div>
+            </li>
         );
     });
 
     return (
-        <ul className="ln-member-list">
+        <ul className="ln-member-list ln-scroll-y ln-scroll-fixed">
             {list}
         </ul>
     )
