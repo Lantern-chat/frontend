8b4eb93237226aed55fec7c5fe7b5ff6fdc55352
novacrazy <novacrazy@gmail.com>
2021-07-07 17:24:32 -0500

    WIP party header

--- src/ui/views/main/components/party/party_header.tsx
+++ src/ui/views/main/components/party/party_header.tsx
@@ -1,14 +1,31 @@
 import React from "react";
+import { useSelector } from "react-redux";
+import { createSelector } from "reselect";
+import { RootState } from "state/root";
 
 import { Glyphicon } from "ui/components/common/glyphicon";
 
 import Balloons from "icons/glyphicons-pro/glyphicons-basic-2-4/svg/individual-svg/glyphicons-basic-883-balloons.svg";
 
+const party_selector = createSelector(
+    (state: RootState) => state.chat.active_party,
+    (state: RootState) => state.party.parties,
+    (active_party, parties) => {
+        return active_party && parties.get(active_party);
+    }
+)
+
 import "./party_header.scss";
 export const PartyHeader = React.memo(() => {
+    let party = useSelector(party_selector);
+
+    if(!party) return null;
+
     return (
         <header className="ln-party-header">
-            Party Header
+            <div className="ln-party-header__name">
+                <span>{party.party.name}</span>
+            </div>
             <Glyphicon src={Balloons} />
         </header>
     );
