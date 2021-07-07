29952ae04e66cb54abff2f3843aa389f364a2476
novacrazy <novacrazy@gmail.com>
2021-07-07 17:24:48 -0500

    Cleanup channel listing and close panel if open on navigate

--- src/ui/views/main/components/party/channel_list.tsx
+++ src/ui/views/main/components/party/channel_list.tsx
@@ -1,6 +1,10 @@
 import React from "react";
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { createSelector, createStructuredSelector } from "reselect";
+
+import { RootState, Type } from "state/root";
+import { Room } from "state/models";
+import { Panel } from "state/reducers/window";
 
 import { Bounce } from "ui/components/common/spinners/bounce";
 import { Glyphicon } from "ui/components/common/glyphicon";
@@ -8,24 +12,6 @@ import { Avatar } from "ui/components/common/avatar";
 import { Link } from "ui/components/history";
 
 import Hash from "icons/glyphicons-pro/glyphicons-basic-2-4/svg/individual-svg/glyphicons-basic-740-hash.svg";
-
-import { RootState } from "state/root";
-import { Room } from "state/models";
-
-
-const ListedChannel = React.memo(({ room }: { room: Room }) => {
-    return (
-        <Link className="ln-channel-list__channel" href={`/channels/${room.party_id || '@me'}/${room.id}`}>
-            <div className="ln-channel-list__icon">
-                {room.icon_id ?
-                    <Avatar url={`/avatars/${room.id}/${room.icon_id}`} username={room.name} /> :
-                    <Glyphicon src={Hash} />}
-            </div>
-            <div className="ln-channel-list__name"><span>{room.name}</span></div>
-        </Link>
-    );
-});
-
 
 let channel_list_selector = createSelector(
     (state: RootState) => state.chat.active_party, // party_id
@@ -44,6 +30,14 @@ let channel_list_selector = createSelector(
 import "./channel_list.scss";
 export const ChannelList = React.memo(() => {
     let { selected, rooms = [] } = useSelector(channel_list_selector);
+    let show_panel = useSelector((state: RootState) => state.window.show_panel);
+    let dispatch = useDispatch();
+
+    // if on the room list, toggle out of that on selection of a room.
+    let on_navigate: undefined | (() => void);
+    if(show_panel == Panel.LeftRoomList) {
+        on_navigate = () => { dispatch({ type: Type.WINDOW_TOGGLE_ROOM_LIST_SIDEBAR }); };
+    }
 
     let inner = rooms.length == 0 ?
         <div style={{ height: "100%", paddingTop: '1em' }}>
@@ -51,7 +45,14 @@ export const ChannelList = React.memo(() => {
         </div> :
         rooms.map(room =>
             <li key={room.id} className={room.id == selected ? 'selected' : undefined}>
-                <ListedChannel room={room} />
+                <Link className="ln-channel-list__channel" href={`/channels/${room.party_id || '@me'}/${room.id}`} onNavigate={on_navigate}>
+                    <div className="ln-channel-list__icon">
+                        {room.icon_id ?
+                            <Avatar url={`/avatars/${room.id}/${room.icon_id}`} username={room.name} /> :
+                            <Glyphicon src={Hash} />}
+                    </div>
+                    <div className="ln-channel-list__name"><span>{room.name}</span></div>
+                </Link>
             </li>
         );
 
