be86d92acc11f9297f6307feca72fb48abd56492
novacrazy <novacrazy@gmail.com>
2021-09-14 12:11:27 -0500

    Debounce prefs updates

--- src/state/commands/prefs.ts
+++ src/state/commands/prefs.ts
@@ -4,17 +4,27 @@ import { DispatchableAction, Type } from "state/actions";
 import { UserPreferences } from "state/models";
 import { RootState } from "state/root";
 
+var temp_prefs: Partial<UserPreferences> = {};
+var dispatch_timeout: number;
+
 export function savePrefs(prefs: Partial<UserPreferences>): DispatchableAction {
     return async (dispatch, getState) => {
-        let state = getState();
-
-        let res = await fetch({
-            url: '/api/v1/user/@me/prefs',
-            method: XHRMethod.PATCH,
-            bearer: state.user.session!.auth,
-            json: prefs,
-        });
+        temp_prefs = { ...temp_prefs, ...prefs };
 
         dispatch({ type: Type.UPDATE_PREFS, prefs });
+
+        clearTimeout(dispatch_timeout);
+        dispatch_timeout = setTimeout(async () => {
+            let state = getState();
+
+            let res = await fetch({
+                url: '/api/v1/user/@me/prefs',
+                method: XHRMethod.PATCH,
+                bearer: state.user.session!.auth,
+                json: temp_prefs,
+            });
+
+            temp_prefs = {};
+        }, 500);
     };
 }
