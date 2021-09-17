f2340e904758b512bbf1646a60529217032a1d62
novacrazy <novacrazy@gmail.com>
2021-09-17 11:57:02 -0500

    WIP feed loading on scroll

--- src/ui/views/main/components/channel/message/feed.tsx
+++ src/ui/views/main/components/channel/message/feed.tsx
@@ -1,4 +1,4 @@
-import React, { useContext, useRef, useMemo, useState, useEffect, useCallback, useReducer } from "react";
+import React, { useContext, useRef, useMemo, useState, useEffect, useCallback, useReducer, useLayoutEffect } from "react";
 import { useSelector, shallowEqual, useDispatch } from "react-redux";
 import { createStructuredSelector } from 'reselect';
 
@@ -6,7 +6,7 @@ import { useResizeDetector } from "react-resize-detector/build/withPolyfill";
 
 import dayjs from "lib/time";
 
-import { Attachment, Snowflake, Message as MessageModel } from "state/models";
+import { Attachment, Snowflake, Message as MessageModel, Room } from "state/models";
 import { RootState, Type } from "state/root";
 import { IChatState, IWindowState } from "state/reducers";
 import { IMessageState } from "state/reducers/chat";
@@ -127,49 +127,16 @@ const feed_selector = createStructuredSelector({
     show_panel: (state: RootState) => state.window.show_panel,
 });
 
-enum ScrollAnchor {
-    Top,
-    Scrolling,
-    Bottom,
-}
-
-interface IScrollState {
-    pos: number,
-    anchor: ScrollAnchor,
-}
-
-const SCROLL_STATE: IScrollState = {
-    anchor: ScrollAnchor.Bottom,
-    pos: 0,
-};
-
-enum ScrollActionType {
-    Scroll,
-}
-
-interface ScrollActionScroll {
-    type: ScrollActionType.Scroll,
-    payload: number,
-}
-
-type ScrollAction = ScrollActionScroll;
-
-function scrollStateReducer(state: IScrollState, action: ScrollAction): IScrollState {
-    switch(action.type) {
-        case ScrollActionType.Scroll: return { ...state, pos: action.payload };
-    }
-
-    return state;
-}
 
 import "./feed.scss";
+import { loadMessages, SearchMode } from "state/commands";
 export const MessageFeed = React.memo((props: IMessageListProps) => {
     let dispatch = useDispatch();
 
     let room = useSelector((state: RootState) => state.chat.rooms.get(props.channel));
     let { is_light_theme, use_mobile_view, show_panel } = useSelector(feed_selector);
 
-    let groups: IMessageState[][] = useMemo(() => {
+    let groups: Array<IMessageState[]> = useMemo(() => {
         if(room == null) return [];
 
         let groups: IMessageState[][] = [];
@@ -192,60 +159,6 @@ export const MessageFeed = React.memo((props: IMessageListProps) => {
         return groups;
 
     }, [room?.msgs]);
-
-    let container_ref = useRef<HTMLDivElement>(null);
-    const { width, height, ref: ul_ref } = useResizeDetector<HTMLUListElement>();
-
-    let [state, scrollDispatch] = useReducer(scrollStateReducer, SCROLL_STATE);
-
-    let on_scroll = useCallback(throttle((event: React.UIEvent<HTMLDivElement>) => {
-        let t = event.currentTarget;
-        if(!t) return;
-
-        let at_top = t.scrollTop == 0, at_bottom = (t.scrollTop == (t.scrollHeight - t.offsetHeight));
-
-        //if(use_mobile_view) {
-        //    let delta = scrollTop - t.scrollTop;
-        //    if(delta != 0 && (at_top || at_bottom)) {
-        //        event.preventDefault();
-        //    }
-        //}
-
-        scrollDispatch({ type: ScrollActionType.Scroll, payload: t.scrollTop });
-    }, 100), []); // TODO: This will probably have dependencies
-
-    let feed = useMemo(() => {
-        if(!room) {
-            return <div className="ln-center-standalone">Channel does not exist</div>;
-        }
-
-        let wrapperClasses = "ln-msg-list__wrapper ln-scroll-y",
-            MaybeTimeline: React.FunctionComponent<ITimelineProps> = Timeline;
-
-        if(use_mobile_view) {
-            MaybeTimeline = () => <></>;
-        } else {
-            wrapperClasses += ' has-timeline';
-        }
-
-        return (
-            <>
-                <MaybeTimeline direction={0} position={0} />
-                <div className={wrapperClasses} onScroll={on_scroll} ref={container_ref}>
-                    <ul className="ln-msg-list" ref={ul_ref}>
-                        {groups.map(group => <MessageGroup key={group[0].msg.id} group={group} is_light_theme={is_light_theme} />)}
-                    </ul>
-                </div>
-            </>
-        );
-    }, [groups, ul_ref.current, on_scroll]);
-
-    useEffect(() => {
-        let elem = container_ref.current;
-        if(elem) {
-            elem.scrollTo({ top: elem.scrollHeight });
-        }
-    }, [height, container_ref.current]);
 
     let cover;
     if(use_mobile_view && show_panel != Panel.Main) {
@@ -270,7 +183,170 @@ export const MessageFeed = React.memo((props: IMessageListProps) => {
     return (
         <div className="ln-msg-list__flex-container">
             {cover}
-            {feed}
+            <MessageFeedInner room={!!room} groups={groups} use_mobile_view={use_mobile_view} is_light_theme={is_light_theme} />
         </div>
     );
 });
+
+enum ScrollAnchor {
+    Top,
+    Scrolling,
+    Bottom,
+}
+
+interface IScrollState {
+    pos: number,
+    anchor: ScrollAnchor,
+    height: number,
+    old_height: number,
+}
+
+const SCROLL_STATE: IScrollState = {
+    anchor: ScrollAnchor.Bottom,
+    pos: 0,
+    height: 0,
+    old_height: 0,
+};
+
+enum ScrollActionType {
+    Scroll,
+    HeightChange,
+}
+
+interface ScrollActionScroll {
+    type: ScrollActionType.Scroll,
+    pos: number,
+    anchor: ScrollAnchor
+}
+
+interface ScrollActionHeightChange {
+    type: ScrollActionType.HeightChange,
+    h: number,
+    pos: number,
+}
+
+type ScrollAction = ScrollActionScroll | ScrollActionHeightChange;
+
+function scrollStateReducer(state: IScrollState, action: ScrollAction): IScrollState {
+    switch(action.type) {
+        case ScrollActionType.Scroll: {
+            __DEV__ && console.log("ANCHOR SET TO: ", ScrollAnchor[action.anchor]);
+            return { ...state, pos: action.pos, anchor: action.anchor };
+        }
+        case ScrollActionType.HeightChange: return { ...state, height: action.h, old_height: action.h != state.height ? state.height : state.old_height, pos: action.pos };
+    }
+
+    return state;
+}
+
+
+interface IMessageFeedInnerProps {
+    room: boolean,
+    groups: Array<IMessageState[]>,
+    use_mobile_view: boolean,
+    is_light_theme: boolean,
+}
+
+function compute_at(e: HTMLElement, top_threshold: number, bottom_threshold: number): ScrollAnchor {
+    let scroll_height = e.scrollHeight - e.offsetHeight,
+        scroll_top = e.scrollTop;
+
+    if(scroll_top < top_threshold) {
+        return ScrollAnchor.Top;
+    } else if((scroll_height - scroll_top) < bottom_threshold) {
+        return ScrollAnchor.Bottom;
+    } else {
+        return ScrollAnchor.Scrolling;
+    }
+}
+
+const inner_selector = createStructuredSelector({
+    active_room: (state: RootState) => state.chat.active_room,
+    window_height: (state: RootState) => state.window.height,
+})
+
+function debounce<T, R>(cb: (x: T) => R): (x: T) => R {
+    return (x: T) => {
+        return cb(x);
+    };
+}
+
+const MessageFeedInner = React.memo(({ room, groups, use_mobile_view, is_light_theme }: IMessageFeedInnerProps) => {
+    let container_ref = useRef<HTMLDivElement>(null);
+
+    let [state, scrollDispatch] = useReducer(scrollStateReducer, SCROLL_STATE);
+
+    let on_resize = useCallback((_width: number | undefined, height: number | undefined) => {
+        let container = container_ref.current;
+        if(container && height !== undefined) {
+            let top = state.pos;
+            if(state.anchor == ScrollAnchor.Bottom) {
+                top = container.scrollHeight;
+                __DEV__ && console.log("SCROLLING TO BOTTOM: ", top);
+                container.scrollTo({ top });
+            } else {
+                //console.log("HEIGHTS: ", container.scrollHeight, list.clientHeight, state.height, state.old_height);
+
+                console.log("HEIGHTS: ", container.scrollHeight, height, state.height, state.pos, container.scrollTop);
+
+                let diff = height - state.height;
+                top = top + diff;
+                container.scrollTo({ top });
+            }
+
+            scrollDispatch({ type: ScrollActionType.HeightChange, h: height, pos: top });
+        }
+    }, [state, container_ref.current]);
+
+    const { ref: ul_ref } = useResizeDetector<HTMLUListElement>({
+        onResize: on_resize,
+    });
+
+    let { active_room, window_height } = useSelector(inner_selector);
+    let dispatch = useDispatch();
+
+    let load_messages = useCallback(throttle(() => {
+        if(active_room && groups[0]) {
+            dispatch(loadMessages(active_room, groups[0][0].msg.id, SearchMode.Before));
+        }
+    }, 500), [active_room, groups]);
+
+    let on_scroll = useCallback((event: React.UIEvent<HTMLDivElement>) => {
+        let t = event.currentTarget;
+        if(!t) return;
+
+        __DEV__ && console.log("SCROLLED");
+
+        let anchor = compute_at(t, window_height * 0.75, window_height * 0.35);
+
+        if(anchor == ScrollAnchor.Top) {
+            load_messages();
+        }
+
+        scrollDispatch({ type: ScrollActionType.Scroll, pos: t.scrollTop, anchor });
+    }, [load_messages, window_height]); // TODO: This will probably have dependencies
+
+    if(!room) {
+        return <div className="ln-center-standalone">Channel does not exist</div>;
+    }
+
+    let wrapperClasses = "ln-msg-list__wrapper ln-scroll-y",
+        MaybeTimeline: React.FunctionComponent<ITimelineProps> = Timeline;
+
+    if(use_mobile_view) {
+        MaybeTimeline = () => <></>;
+    } else {
+        wrapperClasses += ' has-timeline';
+    }
+
+    return (
+        <>
+            <MaybeTimeline direction={0} position={0} />
+            <div className={wrapperClasses} onScroll={on_scroll} ref={container_ref}>
+                <ul className="ln-msg-list" ref={ul_ref}>
+                    {groups.map(group => <MessageGroup key={group[0].msg.id} group={group} is_light_theme={is_light_theme} />)}
+                </ul>
+            </div>
+        </>
+    );
+})
