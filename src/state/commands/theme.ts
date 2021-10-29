bd2f1e4ee5784fd686b24c9ce6d51a78f76bd0f5
novacrazy <novacrazy@gmail.com>
2021-10-29 03:51:33 -0500

    Obey reduce-motion options when changing theme

--- src/state/commands/theme.ts
+++ src/state/commands/theme.ts
@@ -1,13 +1,16 @@
 import { DispatchableAction, Type } from "state/actions";
 import { setTheme as setRealTheme } from "lib/theme";
 import { StorageKey } from "state/storage";
+import { hasUserPrefFlag, UserPreferenceFlags } from "state/models";
 
 var dispatch_timeout: number;
 
 export function setTheme(temperature: number, is_light: boolean): DispatchableAction {
     return (dispatch, getState) => {
+        let reduce_motion = hasUserPrefFlag(getState().prefs, UserPreferenceFlags.ReduceAnimations);
+
         let theme = { temperature, is_light };
-        setRealTheme(theme, true);
+        setRealTheme(theme, !reduce_motion);
 
         // effectively debounces the theme setting in redux, avoiding costly subscription updates
         clearTimeout(dispatch_timeout);
