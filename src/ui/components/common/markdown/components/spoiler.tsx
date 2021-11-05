5774d793787adfb3533e7479eb2f9c474e62b359
novacrazy <novacrazy@gmail.com>
2021-11-05 03:17:04 -0500

    Improve spoiler slightly

--- src/ui/components/common/markdown/components/spoiler.tsx
+++ src/ui/components/common/markdown/components/spoiler.tsx
@@ -2,10 +2,14 @@ import React, { useState } from "react";
 
 import "./spoiler.scss";
 export const Spoiler = React.memo(({ children }: { children: React.ReactNode }) => {
-    let [visible, setVisible] = useState(false);
+    let [visible, setVisible] = useState(false), title;
+
+    if(!visible) {
+        title = "Click to reveal spoiler";
+    }
 
     return (
-        <span onClick={() => setVisible(true)} className={"spoiler " + (visible ? 'visible' : 'hidden')}>
+        <span onClick={() => setVisible(true)} className={"spoiler " + (visible ? 'visible' : 'hidden')} title={title}>
             {children}
         </span>
     )
