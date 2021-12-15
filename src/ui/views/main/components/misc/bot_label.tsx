7cb7f60ccd9319852f3a6917af96b85b25194d32
novacrazy <novacrazy@gmail.com>
2021-12-15 04:04:52 -0600

    Small tweaks

--- src/ui/views/main/components/misc/bot_label.tsx
+++ src/ui/views/main/components/misc/bot_label.tsx
@@ -3,10 +3,10 @@ import React from "react"
 import "./bot_label.scss";
 export const BotLabel = React.memo(() => {
     return (
-        <span className="bot-label">
+        <span className="bot-label" title="Bot">
             <span className="ui-text">Bot</span>
         </span>
-    )
+    );
 });
 
 if(__DEV__) {
