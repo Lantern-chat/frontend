a8375c8bb9f1dc3baf501137301670b983b54add
novacrazy <novacrazy@gmail.com>
2022-02-23 05:41:36 -0600

    Don't set presence when not connected

--- src/state/commands/presence.ts
+++ src/state/commands/presence.ts
@@ -1,6 +1,7 @@
 import { IS_MOBILE } from "lib/user_agent";
 import { DispatchableAction } from "state/actions";
 import { GLOBAL } from "state/global";
+import { GatewayStatus } from "state/reducers/gateway";
 import { GatewayCommandDiscriminator } from "worker/gateway/cmd";
 
 export function setPresence(away: boolean): DispatchableAction {
@@ -8,6 +9,7 @@ export function setPresence(away: boolean): DispatchableAction {
         let state = getState(),
             presence = state.user.presence;
 
+        if(state.gateway.status != GatewayStatus.Connected) return;
         if(presence && (presence.flags & 3) == (away ? 2 : 1)) return;
 
         GLOBAL.gateway!.postCmd({ t: GatewayCommandDiscriminator.SetPresence, away, mobile: IS_MOBILE });
