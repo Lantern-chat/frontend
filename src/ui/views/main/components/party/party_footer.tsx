4b9a84f35f4c875398a828bcd026adf9eeba8f28
novacrazy <novacrazy@gmail.com>
2022-03-12 19:12:57 -0600

    Port Party footer and header

--- src/ui/views/main/components/party/party_footer.tsx
+++ src/ui/views/main/components/party/party_footer.tsx
@@ -1,6 +1,5 @@
-import React, { useState } from "react";
-import { useSelector } from "react-redux";
-import { createSelector, createStructuredSelector } from "reselect";
+import { createSignal, Show } from "solid-js";
+import { useStructuredSelector } from "solid-mutant";
 
 import { RootState } from "state/root";
 import { activeParty } from "state/selectors/active";
@@ -12,83 +11,63 @@ import { VectorIcon } from "ui/components/common/icon";
 import { Link } from "ui/components/history";
 import { Spinner } from "ui/components/common/spinners/spinner";
 
-import { CogwheelIcon } from "lantern-icons";
-import { SpeakerIcon } from "lantern-icons";
-import { SpeakerDeafIcon } from "lantern-icons";
-import { MicrophoneIcon } from "lantern-icons";
-import { MicrophoneMuteIcon } from "lantern-icons";
-
-let status_selector = createSelector(
-    (state: RootState) => state.user.user!,
-    (state: RootState) => state.party.parties,
-    activeParty,
-    (user, parties, active_party) => {
-        if(!active_party || active_party == '@me') return PresenceStatus.Online;
-        let party = parties.get(active_party);
-
-        if(party) {
-            let member = party.members.get(user.id);
-
-            if(member) {
-                return parse_presence(member.presence).status;
-            }
-        }
-
-        return PresenceStatus.Offline;
-    }
-)
-
-let footer_selector = createStructuredSelector({
-    user: (state: RootState) => state.user.user,
-    is_light_theme: selectPrefsFlag(UserPreferenceFlags.LightMode),
-    status: status_selector,
-});
+import { CogwheelIcon, SpeakerIcon, SpeakerDeafIcon, MicrophoneIcon, MicrophoneMuteIcon } from "lantern-icons";
 
 import "./party_footer.scss";
-export const PartyFooter = React.memo(() => {
-    let { user, is_light_theme, status } = useSelector(footer_selector),
-        [mute, setMute] = useState(false),
-        [deaf, setDeaf] = useState(false),
-        user_info;
+export function PartyFooter() {
+    let [mute, setMute] = createSignal(false),
+        [deaf, setDeaf] = createSignal(false);
 
-    if(user) {
-        user_info = (
-            <>
-                <UserAvatar nickname={user.username} user={user} status={status} is_light_theme={is_light_theme} />
+    let state = useStructuredSelector({
+        user: (state: RootState) => state.user.user,
+        is_light_theme: selectPrefsFlag(UserPreferenceFlags.LightMode),
+        status: (state: RootState) => {
+            let active_party = activeParty(state), party, member;
 
-                <div className="ln-username">
-                    <span className="ln-username__name ui-text">
-                        {user.username}
-                    </span>
-                    <span className="ln-username__discrim ui-text">
-                        #{user.discriminator.toString(16).toUpperCase().padStart(4, '0')}
-                    </span>
-                </div>
-            </>
-        );
-    } else {
-        user_info = (
-            <Spinner size="100%" />
-        );
-    }
+            if(!active_party || active_party == '@me') return PresenceStatus.Online;
+
+            if(party = state.party.parties[active_party]) {
+                if(member = party.members[state.user.user!.id]) {
+                    return parse_presence(member.presence).status;
+                }
+            }
+
+            return PresenceStatus.Offline;
+        }
+    });
 
     return (
         <footer className="ln-party-footer">
             <div className="ln-party-footer__user">
-                {user_info}
+                <Show when={state.user} fallback={<Spinner size="100%" />}>
+                    {user => (<>
+                        <UserAvatar nickname={user.username} user={user} status={state.status} is_light_theme={state.is_light_theme} />
+
+                        <div className="ln-username">
+                            <span className="ln-username__name ui-text">
+                                {user.username}
+                            </span>
+                            <span className="ln-username__discrim ui-text">
+                                #{user.discriminator.toString(16).toUpperCase().padStart(4, '0')}
+                            </span>
+                        </div>
+                    </>)}
+                </Show>
             </div>
 
             <div className="ln-party-footer__settings">
-                <div onClick={() => setMute(!mute)} title={mute ? 'Unmute' : 'Mute'}>
-                    <VectorIcon src={mute ? MicrophoneMuteIcon : MicrophoneIcon} />
+                <div onClick={() => setMute(v => !v)} title={mute() ? 'Unmute' : 'Mute'}>
+                    <VectorIcon src={mute() ? MicrophoneMuteIcon : MicrophoneIcon} />
                 </div>
-                <div onClick={() => setDeaf(!deaf)} title={deaf ? 'Undeafen' : 'Deafen'}>
-                    <VectorIcon src={deaf ? SpeakerDeafIcon : SpeakerIcon} />
+
+                <div onClick={() => setDeaf(v => !v)} title={deaf() ? 'Undeafen' : 'Deafen'}>
+                    <VectorIcon src={deaf() ? SpeakerDeafIcon : SpeakerIcon} />
                 </div>
+
                 <Link href="/settings" title="Settings">
                     <VectorIcon src={CogwheelIcon} />
                 </Link>
             </div>
         </footer>
     );
-})
+}
--- src/ui/views/main/components/party/party_header.tsx
+++ src/ui/views/main/components/party/party_header.tsx
@@ -1,33 +1,32 @@
-import React from "react";
-import { useSelector } from "react-redux";
-import { createSelector } from "reselect";
-import { RootState } from "state/root";
+import { Show } from "solid-js";
+import { useRootSelector } from "state/root";
 import { activeParty } from "state/selectors/active";
 
 import { VectorIcon } from "ui/components/common/icon";
 
 import { BalloonsIcon } from "lantern-icons";
 
-const party_selector = createSelector(
-    activeParty,
-    (state: RootState) => state.party.parties,
-    (active_party, parties) => {
-        return active_party && parties.get(active_party);
-    }
-)
-
 import "./party_header.scss";
-export const PartyHeader = React.memo(() => {
-    let party = useSelector(party_selector);
-
-    if(!party) return null;
+export function PartyHeader() {
+    let party = useRootSelector(state => {
+        let active_party = activeParty(state);
+        if(active_party) {
+            return state.party.parties[active_party];
+        }
+        return;
+    });
 
     return (
-        <header className="ln-party-header">
-            <div className="ln-party-header__name">
-                <span className="ui-text">{party.party.name}</span>
-            </div>
-            <VectorIcon src={BalloonsIcon} />
-        </header>
+        <Show when={party()}>
+            {party => (
+                <header className="ln-party-header">
+                    <div className="ln-party-header__name">
+                        <span className="ui-text">{party.party.name}</span>
+                    </div>
+
+                    <VectorIcon src={BalloonsIcon} />
+                </header>
+            )}
+        </Show>
     );
-});
+}
