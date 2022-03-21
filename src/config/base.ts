0198db75813c5c6cd8c5889cbe8093836696ce78
novacrazy <novacrazy@gmail.com>
2022-03-20 19:09:15 -0500

    Increase nav timeout slightly

--- src/config/base.ts
+++ src/config/base.ts
@@ -1,4 +1,4 @@
 export const BASE_URL = "https://lantern.chat";
 export const CDN_URL = "https://cdn.lanternchat.net";
 
-export const NAV_TIMEOUT: number = 125;
+export const NAV_TIMEOUT: number = 220;
