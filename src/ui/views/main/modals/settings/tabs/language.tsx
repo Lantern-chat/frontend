2c84210666479c581c4c20407799737105adcfa2
novacrazy <novacrazy@gmail.com>
2022-04-05 21:29:38 -0500

    Loading spinner for loading locales

--- src/ui/views/main/modals/settings/tabs/language.tsx
+++ src/ui/views/main/modals/settings/tabs/language.tsx
@@ -1,4 +1,4 @@
-import { createSelector, For } from "solid-js";
+import { createSelector, createSignal, For, Show } from "solid-js";
 import type { Locales } from "ui/i18n/i18n-types";
 
 export const LanguageSettingsTab = () => {
@@ -13,18 +13,31 @@ import { LANGUAGES, LANGUAGE_KEYS, useLocale } from "ui/i18n";
 import { loadLocaleAsync, loadNamespaceAsync } from "ui/i18n/i18n-util.async";
 
 import "./language.scss";
+import { Spinner } from "ui/components/common/spinners/spinner";
 function LangPicker() {
     let { locale, setLocale } = useLocale();
 
-    let selected = createSelector(locale);
+    let [loading, setLoading] = createSignal<undefined | Locales>();
+
     let on_select = async (which: Locales) => {
+        if(loading() || which == locale()) return;
+
+        setLoading(which);
+
         await Promise.all([
             loadLocaleAsync(which),
             loadNamespaceAsync(which, 'main'),
+            //__DEV__ && new Promise(resolve => {
+            //    setTimeout(() => resolve(null), 1000000);
+            //}),
         ]);
 
         setLocale(which);
+        setLoading();
     };
+
+    let selected = createSelector(locale);
+    let selecting = createSelector(loading);
 
     return (
         <ul className="lang-list">
@@ -40,6 +53,11 @@ function LangPicker() {
                         >
                             <div className="lang-emoji">{lang.e}</div>
                             <div className="lang-name">{lang.n}</div>
+                            <Show when={selecting(key)}>
+                                <div className="lang-spinner">
+                                    <Spinner size="1.5em"></Spinner>
+                                </div>
+                            </Show>
                         </li>
                     )
                 }}
