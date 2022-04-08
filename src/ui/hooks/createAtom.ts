a729a879e65302ff64783a88de0c1ce18b73c1d1
novacrazy <novacrazy@gmail.com>
2022-04-07 21:44:39 -0500

    Add createAtom if it's ever needed

---
+++ src/ui/hooks/createAtom.ts
@@ -0,0 +1,12 @@
+import type { Accessor, Setter } from "solid-js";
+import { createSignal } from "solid-js";
+
+export type Atom<T> = Accessor<T> & Setter<T>;
+
+export function createAtom<T>(initial?: T): Atom<T | undefined>;
+export function createAtom<T>(initial: T): Atom<T>;
+
+export function createAtom<T>(initial?: T): Atom<T> {
+    let [value, setValue] = createSignal(initial);
+    return (...args: any[]): any => args.length ? setValue(args[0]) : value();
+}
