1707cb0d5b7eb54fe26794d1f31159bfa364877a
novacrazy <novacrazy@gmail.com>
2022-06-20 10:23:07 -0500

    Improve user cards

--- src/ui/views/main/components/menus/user_card.tsx
+++ src/ui/views/main/components/menus/user_card.tsx
@@ -1,15 +1,17 @@
-import { createMemo, Match, Show, Switch } from "solid-js";
+import { createMemo, Show } from "solid-js";
 import { useStructuredSelector } from "solid-mutant";
-import { PartyMember, Snowflake, User, PresenceStatus, parse_presence, UserPreferenceFlags } from "state/models";
+import { PartyMember, Snowflake, User, parse_presence, UserPreferenceFlags, user_is_bot } from "state/models";
 import { ReadRootState, useRootSelector } from "state/root";
 import { activeParty } from "state/selectors/active";
 import { selectCachedUser } from "state/selectors/selectCachedUser";
-import { Avatar } from "ui/components/common/avatar";
 import { useI18nContext } from "ui/i18n/i18n-solid";
 import { copyText } from "lib/clipboard";
-import { VectorIcon } from 'ui/components/common/icon';
-
-import { Icons } from "lantern-icons";
+import { selectPrefsFlag } from "state/selectors/prefs";
+import { pickColorFromHash } from "lib/palette";
+import { Markdown } from "ui/components/common/markdown";
+import { Branch } from "ui/components/flow";
+import { UserAvatar } from "../user_avatar";
+import { BotLabel } from "../misc/bot_label";
 
 export interface IUserCardProps {
     user: User,
@@ -19,10 +21,6 @@ export interface IUserCardProps {
 
 import "./list.scss";
 import "./user_card.scss";
-import { selectPrefsFlag } from "state/selectors/prefs";
-import { pickColorFromHash } from "lib/palette";
-import { Markdown } from "ui/components/common/markdown";
-import { user_avatar_url } from "config/urls";
 export function UserCard(props: IUserCardProps) {
     const { LL } = useI18nContext();
 
@@ -31,8 +29,8 @@ export function UserCard(props: IUserCardProps) {
         return selectCachedUser(state, props.user.id, active_party);
     });
 
-    let user_discriminator = createMemo(() => props.user.discriminator.toString(16).toUpperCase().padStart(4, '0')) ;
-    
+    let discriminator = createMemo(() => props.user.discriminator.toString(16).toUpperCase().padStart(4, '0'));
+
     let state = useStructuredSelector({
         is_light_theme: selectPrefsFlag(UserPreferenceFlags.LightMode),
         party: (state: ReadRootState) => {
@@ -44,74 +42,57 @@ export function UserCard(props: IUserCardProps) {
         },
     });
 
-    
     let member_info = createMemo(() => state.party?.members[props.user.id]);
-    let background_color = createMemo(() => pickColorFromHash(props.user.id, state.is_light_theme));
     let presence = createMemo(() => parse_presence(member_info()?.presence));
-
-    let status = createMemo(() => {
-        switch(presence().status) {
-            case PresenceStatus.Online: return [LL().main.ONLINE(), "online"];
-            case PresenceStatus.Busy: return [LL().main.BUSY(), "busy"];
-            case PresenceStatus.Away: return [LL().main.AWAY(), "away"];
-            default: return [LL().main.OFFLINE(), "offline"];
-        }
-    });
-
     let nick = createMemo(() => member_info()?.nick);
 
-    let url_or_color = createMemo(() => {
-        let url, backgroundColor;
-        let user = member_info()!.user;
-        if(user.avatar){
-            url = user_avatar_url(user.id!, user.avatar!);
-        }else{
-            backgroundColor = pickColorFromHash(props.user.id, state.is_light_theme);
-        }
-        return { url, backgroundColor };
-    })
-
-    
     return (
         <Show when={cached_user()} fallback={<span class="ui-text">User Not Found</span>}>
-            {cached_user => {
-                return (
-                    <>
+            {cached_user => (
                 <div class="ln-user-card ln-contextmenu">
-                    <div class="banner" style={{"background-color":background_color()}}></div>
-                    <Avatar username={props.user.username} text={(nick() ?? cached_user.user.username)?.charAt(0)} {...url_or_color()} rounded={true} />
-                    <div class="ln-user-status" title={status()[0]}>
-                        <Show
-                            fallback={<span class={status()[1]} />}
-                            when={presence().status == PresenceStatus.Online && presence().is_mobile}
-                        >
-                            <VectorIcon id={Icons.MobilePhone} />
+                    <div class="ln-user-card__header">
+                        <div class="banner"
+                            style={{ "background-color": pickColorFromHash(props.user.id, state.is_light_theme) }}
+                        />
+
+                        <UserAvatar nickname={nick() || cached_user.user.username}
+                            user={cached_user.user}
+                            status={presence().status}
+                            is_light_theme={state.is_light_theme}
+                            is_mobile={presence().is_mobile} />
+                    </div>
+
+                    <div class="ln-user-card__info">
+                        <div class="ui-text ln-username" onClick={() => copyText(props.user.username + '#' + discriminator())}>
+                            <Branch>
+                                <Branch.If when={nick()}>
+                                    <h4>{nick()}</h4>
+                                    <span>{props.user.username}<span class="ln-username__discriminator">#{discriminator()}</span></span>
+                                </Branch.If>
+                                <Branch.Else>
+                                    <h4>{props.user.username}<span class="ln-username__discriminator">#{discriminator()}</span></h4>
+                                </Branch.Else>
+                            </Branch>
+                            <Show when={user_is_bot(props.user)}>
+                                <BotLabel />
+                            </Show>
+                        </div>
+
+                        <Show when={cached_user.user.status}>
+                            {status => <div class="ln-user-custom-status" textContent={status} />}
+                        </Show>
+
+                        <Show when={cached_user.user.bio}>
+                            <hr />
+                            <div class="ln-user-biography">
+                                <span class="ln-user-biography__title">ABOUT ME</span>
+
+                                <Markdown source={cached_user.user.bio!} />
+                            </div>
                         </Show>
                     </div>
-                    <div class="ln-username" onClick={() => copyText(cached_user.user.username + '#'+user_discriminator)}>
-                        <Switch>
-                            <Match when={nick()}>
-                            <h4 class="ui-font">{nick}</h4>
-                                <span class="ln-username__discrim">{cached_user.user.username} #{user_discriminator}</span>
-                            </Match>
-                            <Match when={!nick()}>
-                                <div class="font-large"><span class="ui-font">{cached_user.user.username}</span> <span class="ln-username__discrim">#{user_discriminator}</span></div>
-                            </Match>
-                        </Switch>
-                    </div>
-                    <Show when={cached_user.user.status}>
-                        {status => <div class="ln-user-custom-status" textContent={status}/>}
-                    </Show>
-                    <Show when={cached_user.user.bio}>
-                        <div class="divider"></div>
-                        <div class="ln-user-biography">
-                            <strong class="ln-user-biography-title">ABOUT ME</strong>
-                            <Markdown source={cached_user.user.bio!} />
-                        </div>
-                    </Show>
-                </div></>
-                )
-            }}
+                </div>
+            )}
         </Show>
     );
 }
