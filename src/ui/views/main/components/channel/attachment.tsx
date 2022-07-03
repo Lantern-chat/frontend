0a31f18cd9a81f19a3791444e078123d55f97dfb
Justin Maximillian Kimlim <kimlimjustin@gmail.com>
2022-07-03 23:03:02 +0700

    refactor: code cleanups

--- src/ui/views/main/components/channel/attachment.tsx
+++ src/ui/views/main/components/channel/attachment.tsx
@@ -5,12 +5,12 @@ const Attachment = (props: IChannelProps) => {
     return (
         <div class="ln-attachment-area">
             {props.attaching_files.map(file => (
-            <div class="ln-channel-attachment" onClick={() => props.remove_attaching_file(file)}>
-                <div class="ln-channel-attachment-delete-btn"> {/*TODO: delete button icon*/}
+            <div class="ln-channel-attachment">
+                <div class="ln-channel-attachment__delete-btn" onClick={() => props.remove_attaching_file(file)}> {/*TODO: delete button icon*/}
                     &times;
                 </div>
                 <MediaPreview file={file} />
-                <div class="ln-channel-attachment-file-name"><span>{file.name}</span></div>
+                <div class="ln-channel-attachment__file-name"><span>{file.name}</span></div>
             </div>
             ))}
         </div>
