b07bdfaa1dd44a0247eff4d4560ad44db75aed1c
novacrazy <novacrazy@gmail.com>
2022-07-21 05:24:17 -0500

    Add preliminary emoji component

---
+++ src/ui/components/common/emoji.tsx
@@ -0,0 +1,40 @@
+import { UserPreferenceFlags } from "state/models";
+import { createEffect } from "solid-js"
+import { useRootSelector } from "state/root";
+import { selectPrefsFlag } from "state/selectors/prefs";
+import { createRef, Ref } from "ui/hooks/createRef";
+import { Branch } from "./../flow";
+
+export interface IEmojiProps {
+    value: string,
+    ui?: boolean,
+}
+
+export function Emoji(props: IEmojiProps) {
+    let ref: HTMLSpanElement | ((el: HTMLElement) => void) | undefined;
+
+    // zero-cost easter egg
+    if(!props.ui && props.value == '🍪') {
+        ref = createRef();
+        createEffect(() => {
+            let r = (ref as Ref<HTMLElement>).current, c = 0;
+            r && r.addEventListener('click', () => {
+                if(++c == 5) { window.open('https://orteil.dashnet.org/cookieclicker/'); }
+            });
+        });
+    }
+
+    let use_system = useRootSelector(selectPrefsFlag(UserPreferenceFlags.UsePlatformEmojis));
+
+    // TODO: Resolve emoji to twemoji file
+    return (
+        <Branch>
+            <Branch.If when={use_system() || true}>
+                <span class="emoji" textContent={props.value} ref={ref} />
+            </Branch.If>
+            <Branch.Else>
+                Twemoji
+            </Branch.Else>
+        </Branch>
+    );
+}
