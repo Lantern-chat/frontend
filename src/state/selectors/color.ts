7ca118d8968a419a2bee7410585a24ae667de2e5
novacrazy <novacrazy@gmail.com>
2022-08-07 02:55:05 -0500

    Tweak color contrast

--- src/state/selectors/color.ts
+++ src/state/selectors/color.ts
@@ -1,13 +1,20 @@
-import { linear2srgb, linear_srgb2oklab, oklab2linear_srgb, pack_rgb, srgb2linear, formatRgbBinary, unpack_rgb, clamp_linear } from "lib/color";
+import { linear2srgb, linear_srgb2oklab, oklab2linear_srgb, pack_rgb, srgb2linear, formatRgbBinary, unpack_rgb, clamp_linear, saturate_lab } from "lib/color";
 import { Accessor, createMemo } from "solid-js";
 import { usePrefs, UserPreferenceAccessors } from "state/contexts/prefs";
 
 export function adjustUserColor(color: number, prefs: UserPreferenceAccessors = usePrefs()): Accessor<number> {
     return () => {
         if(prefs.ForceColorConstrast()) {
-            let lab = linear_srgb2oklab(srgb2linear(unpack_rgb(color)));
+            let lab = linear_srgb2oklab(srgb2linear(unpack_rgb(color))), is_light = prefs.LightMode();
 
-            lab.L = prefs.LightMode() ? Math.min(lab.L, 0.5) : Math.max(lab.L, 0.65);
+            if(prefs.LightMode()) {
+                if(lab.L > 0.6) {
+                    lab.L = 0.6;
+                    lab = saturate_lab(lab, 1.1);
+                }
+            } else {
+                lab.L = Math.max(lab.L, 0.65);
+            }
 
             color = pack_rgb(linear2srgb(clamp_linear(oklab2linear_srgb(lab))));
         }
