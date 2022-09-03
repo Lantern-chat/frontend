72fb576a795073c13b5897c66e09849027699c91
novacrazy <novacrazy@gmail.com>
2022-09-03 08:38:47 -0500

    Simplify spoiler code

--- src/ui/components/common/markdown/components/spoiler.tsx
+++ src/ui/components/common/markdown/components/spoiler.tsx
@@ -1,21 +1,22 @@
-import { createSignal, JSX } from "solid-js";
+import type { JSXElement } from "solid-js";
 import { useI18nContext } from "ui/i18n/i18n-solid";
 
-import "./spoiler.scss";
-export function Spoiler(props: { children: JSX.Element }) {
-    let [visible, setVisible] = createSignal(false);
+function on_spoiler_click(e: MouseEvent) {
+    let el = e.currentTarget as HTMLSpanElement;
+    el.classList.toggle("hidden", false);
+    el.title = '';
+    el.removeEventListener('click', on_spoiler_click);
+}
 
+import "./spoiler.scss";
+export function Spoiler(props: { children: JSXElement }) {
     let { LL } = useI18nContext();
 
     return (
         <span
-            onClick={() => setVisible(true)}
-            class="spoiler"
-            classList={{
-                'visible': visible(),
-                'hidden': !visible(),
-            }}
-            title={visible() ? void 0 : LL().main.SPOILER_TITLE()}
+            on:click={on_spoiler_click}
+            class="spoiler hidden"
+            title={LL().main.SPOILER_TITLE()}
         >
             {/* @once */props.children}
         </span>
