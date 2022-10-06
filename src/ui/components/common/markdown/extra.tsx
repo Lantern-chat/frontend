2b423517cccba58c7d129d1909e0e449c8bfde72
novacrazy <novacrazy@gmail.com>
2022-10-06 03:26:27 -0500

    Remove AST cache

--- src/ui/components/common/markdown/extra.tsx
+++ src/ui/components/common/markdown/extra.tsx
@@ -25,38 +25,19 @@ export const extraRules: ExtraRules = {
 const extraRawParse = parserFor(extraRules);
 const extraSolidOutput: SolidOutput = outputFor(extraRules as any);
 
-interface CachedAst {
-    state: State,
-    ast: SingleASTNode[],
-}
-
-var ASTCache = window['AST_CACHE'] = new Map<string, CachedAst>();
-
 export function SolidMarkdownExtra(props: SolidMarkdownProps): SolidElement {
     let [local, div] = splitProps(props, ['source', 'inline', 'extra']);
 
     let res = () => {
         let state: State = { inline: !!local.inline, extra: local.extra };
 
-        let cached = ASTCache.get(props.source);
-        if(!cached) {
-            let ast = extraRawParse(props.source, state);
+        let ast = extraRawParse(props.source, state);
 
-            if(state.had_emoji && (state.no_text = !has_text(ast))) {
-                ast = clean_text(ast);
-            }
-
-            ASTCache.set(props.source, cached = {
-                state,
-                ast,
-            });
-        } else {
-            state = Object.assign({}, cached.state, state);
-
-            if(ASTCache.size > 200) ASTCache.clear();
+        if(state.had_emoji && (state.no_text = !has_text(ast))) {
+            ast = clean_text(ast);
         }
 
-        return extraSolidOutput(cached.ast, state);
+        return extraSolidOutput(ast, state);
     };
 
     return <div {...div} children={res()} />
